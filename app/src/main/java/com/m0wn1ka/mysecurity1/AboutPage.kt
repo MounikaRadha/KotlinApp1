798a39fa8f29f281b75ef06e4c314f55dbe21160
MounikaRadha <radha.mounika@testsigma.com>
2024-11-22 13:33:26 +0530

    feat/about page added with scrolling

--- app/src/main/java/com/m0wn1ka/mysecurity1/AboutPage.kt
+++ app/src/main/java/com/m0wn1ka/mysecurity1/AboutPage.kt
@@ -1,27 +1,16 @@
 package com.m0wn1ka.mysecurity1
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.m0wn1ka.mysecurity1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var activityMainBinding: ActivityMainBinding
+class AboutPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(activityMainBinding.root)
-//        val url:String="https:///m0wn1ka.github.io"
-//        val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-//        startActivity(urlIntent)
-        activityMainBinding.AboutPageButton.setOnClickListener(){
-            startActivity(Intent(applicationContext,AboutPage::class.java))
-        }
+        setContentView(R.layout.activity_about_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
